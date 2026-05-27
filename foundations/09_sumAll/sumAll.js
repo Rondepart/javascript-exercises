ba35ed0447325f2e0e25fae124c217f0adeb1129
Mark <125941419+Rondepart@users.noreply.github.com>
2026-05-27 22:02:49 +0800

    Add logic to sumAll function, use all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return ("ERROR");
+    if (num1 < 0 || num2 < 0) return ("ERROR");
 
-};
+    let bigNum = (num1 >= num2) ? num1 : num2;
+    let smallNum = (num1 >= num2) ? num2 : num1;
+    let totalSum = 0;
+
+    for (let i = smallNum; smallNum<=bigNum; smallNum++) {
+        totalSum+= smallNum;
+    }
+    return totalSum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
