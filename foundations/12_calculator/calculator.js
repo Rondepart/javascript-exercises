fb7b3927ae2c894cdf3e102855249e8de4681556
Mark <125941419+Rondepart@users.noreply.github.com>
2026-06-29 05:51:11 +0800

    Add code logic to functions to complete exercises, and use all tests to check

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => num + total, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => num * total, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base **= exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (i = 1; i <= num; i++){
+    result *= i;
+  }
+
+  return result
 };
 
 // Do not edit below this line
