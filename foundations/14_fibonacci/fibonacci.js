3e4496f591f150bdace63717ef37af59d94c707e
Mark <125941419+Rondepart@users.noreply.github.com>
2026-07-01 06:01:11 +0800

    Add fibonacci function code logic, and activate all tests for checking

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        let [prev, curr] = [0, 1];
 
+        for (let i=0; i<Number(num); i++) {
+            [prev, curr] = [curr, prev + curr];
+        }
+        return prev
+    } 
 };
 
 // Do not edit below this line
