9ea9b79d72f60f9f23da3030ed1de55ffa6bbf86
Mark <125941419+Rondepart@users.noreply.github.com>
2026-07-01 08:26:33 +0800

    Add code logic to findTheOldest function and new function to get age. Activate all tests for checking

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function (birthDate, deathDate) {
+  if (!deathDate) {
+    deathDate = new Date().getFullYear();
+  }
+  return deathDate - birthDate;
+};
 
+const findTheOldest = function(array) {
+    return array.reduce((oldest, person) =>  {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const age = getAge(person.yearOfBirth, person.yearOfDeath);
+
+        return age > oldestAge ? person : oldest;
+    })
 };
 
 // Do not edit below this line
